51dc04c58acc3c3a797e10daf5e4271a7012d499
DannyD847 <dannydv847@gmail.com>
2025-04-16 20:54:07 -0400

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,6 @@
+original_list = [1,2,3,4,5,6,7,8,9.10]
+extracted = original_list[0:5]
+extracted.reverse()
+print('Original list: ',original_list)
+print('Extracted 5 numbers: ',extracted)
+print('Reversed extracted elements: ',extracted)
